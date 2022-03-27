a8e521fe5103f0301a6f5d6972f0e27ab5a7fbef
Maxim <maximon.api@gmail.com>
2022-03-27 22:29:37 +0400

    Added fields to display the popup "can't scroll" problem

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 ### WebStorm displays the variable type documentation ugly (hover the `User` type to reproduce)
 
+**UPDATE** (27 of March, 2022)
+
+`Hover + Cmd` in Webstorm shows the types identical to formatted JSON in VS Code.
+
+This could solve the problem if only the popup would not become unscrollable.
+See the pic below:
+
+![](./large-type-documentation-in-Webstorm.png)
+
+Also see comments on lines 7 - 9 in [types.ts](./types.ts) to get more context.  
+
+---
+
 **Expected view**: type is beautifully formatted as JSON
 
 **Got view**: type is displayed in one unreadable line
--- types.ts
+++ types.ts
@@ -4,10 +4,35 @@ enum AttachmentType {
     File
 }
 
+// Cmd + Hover "Attachment" to see the popup with scroll bars
+// As soon as you try to scroll this bar the popup closes
+// "field15" is the last item displayed without the scrolling, other are hidden
 type Attachment = {
     userId: number;
     createdAt: Date;
     attachmentType: AttachmentType;
+
+    // Some example fields (alternative to real amount of fields in a real project)
+    field1?: string;
+    field2?: string;
+    field3?: string;
+    field4?: string;
+    field5?: string;
+    field6?: string;
+    field7?: string;
+    field8?: string;
+    field9?: string;
+    field10?: string;
+    field11?: string;
+    field12?: string;
+    field13?: string;
+    field14?: string;
+    field15?: string;
+    field16?: string;
+    field17?: string;
+    field18?: string;
+    field19?: string;
+
 }
 
 type User  = {
@@ -20,7 +45,7 @@ type User  = {
 
     otherData?: {
         someField: any;
-        anotjerField: any;
+        anotherField: any;
     }
 }
 
